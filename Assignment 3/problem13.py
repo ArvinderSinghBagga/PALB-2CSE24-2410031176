11b22648e7a6fd2c1c9a0dd728bd2329ba68d73c
ArvinderSinghBagga <arvinder2830@gmail.com>
2026-02-15 12:26:20 +0530

    Problem14

---
+++ Assignment 3/problem13.py
@@ -0,0 +1,9 @@
+def merge(a, b):
+    n, m = len(a), len(b)
+    i, j = n-1, 0
+    while i >= 0 and j < m and a[i] > b[j]:
+        a[i], b[j] = b[j], a[i]
+        i -= 1; j += 1
+    a.sort()
+    b.sort()
+  
