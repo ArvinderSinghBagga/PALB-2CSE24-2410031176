71842b3df6e1abce1317cbc578636e93932b9f82
ArvinderSinghBagga <arvinder2830@gmail.com>
2026-02-15 11:42:28 +0530

    Update problem2.py

--- Assignment 1/problem2.py
+++ Assignment 1/problem2.py
@@ -1,1 +1,12 @@
-
+def minMax(arr):
+    if not arr:
+        return None
+    min_value = arr[0]
+    max_value = arr[0]
+    for i in range(1, len(arr)):
+        if arr[i] < min_value:
+            min_value = arr[i]
+        elif arr[i] > max_value:
+            max_value = arr[i]
+            
+    return [min_value, max_value]
